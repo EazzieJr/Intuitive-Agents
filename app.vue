058666455cc66c6a2a279fcf76752cc7cd8132a2
Ganiyu Idris <golamide27@gmail.com>
2024-04-24 15:03:16 +0100

    ...

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+	<NuxtLayout>
+		<NuxtLink to="/">
+			<div class="w-full-h-full bg-red-500"></div>
+			<NuxtPage />
+		</NuxtLink>
+	</NuxtLayout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
