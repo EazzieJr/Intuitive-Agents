b5ba127182fb791622f28fb05186e8aa92680a52
Ganiyu Idris <golamide27@gmail.com>
2024-05-02 13:09:28 +0100

    Pushing

--- components/Table.vue
+++ components/Table.vue
@@ -236,7 +236,10 @@ export default {
 		fetching: {
 			type: Boolean,
 			// default: true
-		}
+		},
+		agentDetails: {
+			type: Object
+		},
 	},
 
 	data() {
@@ -287,22 +290,12 @@ export default {
 						label: 'Delete',
 						icon: 'i-heroicons-trash-20-solid',
 						click: () => this.deleteContact(user),
-						class: 'hover:bg-red-500',
-						iconClass: 'fill-red-500 hover:fill-white'
+						class: 'hover:text-white hover:fill-current hover:bg-red-500',
+						iconClass: 'fill-current'
 					}
 				]
 				// Add more items as needed
 			];
-		},
-
-		getAgentName(id) {
-			if (id == "86f0db493888f1da69b7d46bfaecd360") {
-				return "VHD"
-			} else if (id == "214e92da684138edf44368d371da764c") {
-				return "VTE"
-			} else if (id == "0411eeeb12d17a340941e91a98a766d0") {
-				return "RST"
-			}
 		},
 
 		toggleCreateModal(formType, user) {
@@ -323,50 +316,6 @@ export default {
 			}
 
 			this.modalOpened = !this.modalOpened;
-		},
-
-		async getStats(searching) {
-			if (searching) {
-				this.searching = true;
-			}
-
-			try {
-				const pstDate = this.date ? moment(this.date).tz("America/Los_Angeles").format('YYYY-MM-DD') : moment().tz("America/Los_Angeles").format('YYYY-MM-DD');
-				// console.log(this.date, pstDate);
-
-				const response = await fetch("https://retell-backend-yy86.onrender.com/get-stats", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						date: pstDate
-					})
-				});
-
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`);
-				}
-
-				const stats = await response.json(); // Parse the response body as JSON
-
-				console.log(stats);
-
-				this.stats = stats;
-				const logs = {
-					logId1: stats?.logId1 || "",
-					logId2: stats?.logId2 || "",
-					logId3: stats?.logId3 || ""
-				}
-
-				this.fetching = true;
-				this.loadUsers(pstDate);
-				// setTimeout(() => this.refreshNuxtData, 5000)
-			} catch (error) {
-				console.error("Error fetching data:", error);
-				// Return an empty object or handle the error as needed
-				this.stats = {};
-			}
 		},
 
 		loadUsers(page) {
@@ -412,11 +361,24 @@ export default {
 		async callContact(user) {
 			const { _id, phone } = user;
 			try {
-				const response = await axios.post(`https://intuitiveagents.io/create-phone-call/${this.agentId}`, {
-					fromNumber: "+17257268989",
-					toNumber: phone,
-					userId: _id,
+				// Use fetch
+				const response = await fetch(`https://intuitiveagents.io/create-phone-call/${this.agentDetails.id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						fromNumber: "+17257268989",
+						toNumber: phone,
+						userId: _id,
+					})
 				});
+				
+				// const response = await axios.post(`https://intuitiveagents.io/create-phone-call/${this.agentDetails.id}`, {
+				// 	fromNumber: "+17257268989",
+				// 	toNumber: phone,
+				// 	userId: _id,
+				// });
 
 				//console.log("Response: ", response);
 			} catch (err) {
@@ -438,7 +400,7 @@ export default {
 						lastname: lastName,
 						email,
 						phone,
-						agentId: this.agentId
+						agentId: this.agentDetails.id
 					})
 				});
 
@@ -524,7 +486,7 @@ export default {
 			}
 
 			&:nth-child(7) {
-				@apply basis-[16%];
+				@apply basis-[14%];
 			}
 		}
 	}
@@ -568,11 +530,7 @@ export default {
 					}
 
 					&:nth-child(7) {
-						@apply basis-[16%] font-bold;
-					}
-
-					&:nth-child(8) {
-						@apply basis-[6%];
+						@apply basis-[14%] font-bold;
 					}
 				}
 			}
--- pages/dashboard/company/va-group/[name].vue
+++ pages/dashboard/company/va-group/[name].vue
@@ -2,7 +2,7 @@
 	<div class="Dashboard">
 		<header>
 			<h1>
-				Virtual help desk - DANIEL
+				{{ agentDetails.alias }} - {{ agentDetails.name }}
 			</h1>
 
 			<div class="Misc between">
@@ -16,37 +16,7 @@
 					</button>
 				</div>
 
-				<ExportLogs agentId="86f0db493888f1da69b7d46bfaecd360" agentName="Daniel" />
-
-				<!-- <div class="DateFilter start space-x-2">
-					<vue-date-picker v-model="date" mode="date"></vue-date-picker>
-
-					<button class="Download p-[2px] relative w-full max-w-[84px]" @click="filterByDate" :disabled="searching">
-						<div class="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-md" />
-
-						<div
-							class="center px-5 py-1.5 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent text-sm">
-							<span :class="{ 'animate-pulse': searching }" v-if="!searching">
-								Search
-							</span>
-
-							<img class="animate-spin duration-1000 py-1 w-3.5" v-else src="/svg/loading.svg" alt="">
-						</div>
-					</button>
-				</div> -->
-
-				<!-- <button class="Download p-[3px] relative">
-					<div class="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-
-					<div
-						class="start space-x-3.5 px-5 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-						<img src="/svg/download.svg" alt="">
-
-						<span>
-							Download
-						</span>
-					</div>
-				</button> -->
+				<ExportLogs agentId="86f0db493888f1da69b7d46bfaecd360" :agentName="agentDetails.name" />
 			</div>
 
 			<div class="Numbers start">
@@ -63,7 +33,7 @@
 				</h2>
 			</div>
 
-			<Table :users="users" :searching="searching" :totalPages="totalPages" :page="page" @paginate="paginate"
+			<Table :users="users" :searching="searching" :totalPages="totalPages" :page="page" :agentDetails="agentDetails" @paginate="paginate"
 				:fetching="fetching" @setTranscript="setTranscript" @loadUsers="loadUsers" />
 		</div>
 
@@ -80,13 +50,13 @@
 				</div>
 
 				<div class="Body" data-lenis-prevent>
-					<div class="Recording sticky top-0 bg-white mb-5">
+					<div class="Recording sticky top-0 bg-white pb-2.5">
 						<audio class="w-full bg-opacity-0 appearance-none" controls>
 							<source :src="transcript?.recordingUrl" type="audio/wav" />
 						</audio>
 					</div>
 
-					<p v-if="analyzedTranscript" class="pb-5 sticky top-[74px]">
+					<p v-if="analyzedTranscript" class="pb-5 sticky top-[64px] bg-white">
 						<span class="font-semibold">Call Sentiment:</span> {{ analyzedTranscript }}
 					</p>
 
@@ -114,6 +84,7 @@ export default {
 	data() {
 		return {
 			users: [],
+			agentDetails: {},
 
 			transcript: {},
 			analyzedTranscript: {},
@@ -141,65 +112,11 @@ export default {
 	},
 
 	methods: {
-		getAgentName(id) {
-			if (id == "86f0db493888f1da69b7d46bfaecd360") {
-				return "VHD"
-			} else if (id == "214e92da684138edf44368d371da764c") {
-				return "VTE"
-			} else if (id == "0411eeeb12d17a340941e91a98a766d0") {
-				return "RST"
-			}
-		},
-
-		async getStats(searching) {
-			if (searching) {
-				this.searching = true;
-			}
-
-			try {
-				const pstDate = this.date ? moment(this.date).tz("America/Los_Angeles").format('YYYY-MM-DD') : moment().tz("America/Los_Angeles").format('YYYY-MM-DD');
-				// console.log(this.date, pstDate);
-
-				const response = await fetch("https://retell-backend-yy86.onrender.com/get-stats", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						date: pstDate
-					})
-				});
-
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`);
-				}
-
-				const stats = await response.json(); // Parse the response body as JSON
-
-				console.log(stats);
-
-				this.stats = stats;
-				const logs = {
-					logId1: stats?.logId1 || "",
-					logId2: stats?.logId2 || "",
-					logId3: stats?.logId3 || ""
-				}
-
-				this.fetching = true;
-				this.loadUsers(pstDate);
-				// setTimeout(() => this.refreshNuxtData, 5000)
-			} catch (error) {
-				console.error("Error fetching data:", error);
-				// Return an empty object or handle the error as needed
-				this.stats = {};
-			}
-		},
-
 		async loadUsers(page) {
 			// const { logId1, logId2, logId3 } = logs;
 			this.fetching = true;
 			try {
-				const response = await fetch(`https://intuitiveagents.io/users/86f0db493888f1da69b7d46bfaecd360`, {
+				const response = await fetch(`https://intuitiveagents.io/users/${this.agentDetails.id}`, {
 					method: "POST",
 					body: JSON.stringify({
 						limit: 100,
@@ -263,8 +180,27 @@ export default {
 		},
 
 		getAgent() {
-			const { id } = this.$route.params;
-			console.log("Params: ", params);
+			const { name } = this.$route.params;
+
+			if (name == "daniel") {
+				this.agentDetails = {
+					name: "Daniel",
+					alias: "Virtual Help Desk",
+					id: "86f0db493888f1da69b7d46bfaecd360"
+				}
+			} else if (name == "ethan") {
+				this.agentDetails = {
+					name: "Ethan",
+					alias: "Virtual Team Expert",
+					id: "214e92da684138edf44368d371da764c"
+				}
+			} else if (name == "chloe") {
+				this.agentDetails = {
+					name: "Chloe",
+					alias: "Remote Solutions Team",
+					id: "0411eeeb12d17a340941e91a98a766d0"
+				}
+			}
 		}
 	},
 
